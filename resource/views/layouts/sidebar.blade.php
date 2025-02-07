097d36710c9e5cb2938072fecdf8ce04c5ce1f76
Aditya Darma <adhit.boys1@gmail.com>
2025-02-08 06:33:23 +0800

    update

--- resource/views/layouts/sidebar.blade.php
+++ resource/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="index.html"><img src="{{ asset('assets/images/svg/logo.svg') }}" alt="Logo" srcset=""></a>
+                    <a href="index.html"><img src="{{ asset('assets/images/logo/logo.svg') }}" alt="Logo" srcset=""></a>
                 </div>
                 <div class="sidebar-toggler  x">
                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
